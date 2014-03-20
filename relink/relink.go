72db9154458663b6bb24dea00cd59cac73895651
jake <bigjake2s@gmail.com>
2014-03-20 08:59:04 -0400

    Attempt at in-stream replacement

--- relink/relink.go
+++ relink/relink.go
@@ -1,1 +1,60 @@
 package relink
+
+import (
+	"fmt"
+	"strings"
+
+	"bytes"
+	"io"
+)
+
+type Relink struct {
+	domainMap map[string]string
+	buf       *bytes.Buffer
+	w         io.Writer
+	insideUrl bool
+}
+
+var _ io.Writer = new(Relink)
+
+func New(w io.Writer, domains map[string]string) *Relink {
+	return &Relink{
+		domainMap: domains,
+		w:         w,
+		buf:       bytes.NewBuffer(make([]byte, 0, 256)),
+	}
+}
+
+func (r *Relink) Write(p []byte) (n int, err error) {
+	n = len(p)
+	if !r.insideUrl {
+		for idx := bytes.IndexByte(p, 'h'); idx > -1; idx = bytes.IndexByte(p, 'h') {
+			fmt.Printf("I: %s\nI: %s^\n", p, strings.Repeat(" ", idx))
+			switch {
+			case bytes.Equal(p[idx:idx+4] == []byte(`http`):
+				
+			}
+
+			fmt.Printf("Writing to w: %q\n", p[:idx])
+			if _, err := r.w.Write(p[:idx]); err != nil {
+				return n, err
+			}
+			p = p[idx+1:]
+		}
+		// fmt.Printf("Writing to buf: %q\n", p[idx:])
+		// r.buf.Write(p[idx:])
+	}
+	return
+}
+
+// strRe := `https?://([^/]+).?(` + strings.Join(s.TLDs, "|") + `)['"</]`
+// c.Logf("Regexp: %s", strRe)
+// re := regexp.MustCompile(strRe)
+
+// r := io.TeeReader(buf, os.Stdout)
+
+// loc := re.Find(buf.Bytes())
+// c.Logf("%s - %v", test.In, loc)
+// if loc != nil {
+// 	c.Log(strings.Repeat(" ", loc[0]) + "^" + strings.Repeat("-", loc[1]-loc[0]-2) + "^")
+// }
--- relink/relink_test.go
+++ relink/relink_test.go
@@ -1,19 +1,138 @@
 package relink
 
 import (
+	"bytes"
+	"io"
 	"launchpad.net/gocheck"
-	"strings"
 	"testing"
 )
 
-type RelinkSuite struct{}
+type RelinkSuite struct {
+	DomainMap map[string]string
+	Tests     []relinkTest
+}
 
-var _ = gocheck.Suite(new(RelinkSuite))
+type relinkTest struct {
+	In, Out string
+}
+
+var _ = gocheck.Suite(&RelinkSuite{
+	DomainMap: map[string]string{
+		"top.tld": "top.proxy.tld",
+	},
+	Tests: []relinkTest{
+		{
+			In:  `<a href="http://top.tld/">`,
+			Out: `<a href="http://alias.subscription.tld/">`,
+		},
+		{
+			In:  `<a href="http://top.tld">`,
+			Out: `<a href="http://top.tld">`,
+		},
+		{
+			In:  `<a href="http://top.tld/some/page">`,
+			Out: `<a href="http://top.tld/some/page">`,
+		},
+		{
+			In:  `<a href="http://sub.top.tld/some/page">`,
+			Out: `<a href="http://sub.top.tld/some/page">`,
+		},
+		{
+			In:  `<a href="http://sub.sub.top.tld/some/page">`,
+			Out: `<a href="http://sub.sub.top.tld/some/page">`,
+		},
+		{
+			In:  `<a href="http://sub.top.tld">`,
+			Out: `<a href="http://sub.top.tld">`,
+		},
+		{
+			In:  `<link>http://top.tld/</link>`,
+			Out: `<link>http://top.tld/</link>`,
+		},
+		{
+			In:  `<link>http://top.tld</link>`,
+			Out: `<link>http://top.tld</link>`,
+		},
+		{
+			In:  `<link>http://top.tld/some/page</link>`,
+			Out: `<link>http://top.tld/some/page</link>`,
+		},
+		{
+			In:  `<link>http://sub.top.tld/some/page</link>`,
+			Out: `<link>http://sub.top.tld/some/page</link>`,
+		},
+		{
+			In:  `<link>http://sub.sub.top.tld/some/page</link>`,
+			Out: `<link>http://sub.sub.top.tld/some/page</link>`,
+		},
+		{
+			In:  `<link>http://sub.top.tld</link>`,
+			Out: `<link>http://sub.top.tld</link>`,
+		},
+		{
+			In:  `<a href="/some/page">`,
+			Out: `<a href="/some/page">`,
+		},
+		{
+			In:  `<link>/some/page</link>`,
+			Out: `<link>/some/page</link>`,
+		},
+	},
+})
 
 func Test(t *testing.T) { gocheck.TestingT(t) }
 
+func (s *RelinkSuite) testBuf(c *gocheck.C) {
+	buf := bytes.NewBuffer(make([]byte, 0, 1024))
+
+	for _, test := range s.Tests {
+		buf.WriteString(test.In)
+		buf.WriteByte('\n')
+	}
+
+	c.Logf("=== Buffer ===\n%s", buf.Bytes())
+}
+
 func (s *RelinkSuite) TestRelink(c *gocheck.C) {
-	r := strings.NewReader(`
-		
-	`)
+	in := bytes.NewBuffer(make([]byte, 0, 1024))
+
+	for _, test := range s.Tests {
+		in.WriteString(test.In)
+		in.WriteByte('|')
+	}
+
+	outBuf := bytes.NewBuffer(make([]byte, 0, 16))
+	out := New(outBuf, s.DomainMap)
+
+	var err error
+	written := int64(0)
+	buf := make([]byte, 32)
+	for {
+		nr, er := in.Read(buf)
+		if nr > 0 {
+			nw, ew := out.Write(buf[0:nr])
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er == io.EOF {
+			break
+		}
+		if er != nil {
+			err = er
+			break
+		}
+	}
+
+	c.Assert(err, gocheck.IsNil)
+
+	c.Logf("=== In ===\n%s\n=== Out ===\n%s\n", in.Bytes(), outBuf.Bytes())
 }
