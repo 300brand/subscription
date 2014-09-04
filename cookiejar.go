9557ca7b1bf59340d2d805cefad5e24434829644
jake <bigjake2s@gmail.com>
2014-09-04 14:42:54 -0400

    Updated cookiejar storage

--- cookiejar.go
+++ cookiejar.go
@@ -1,34 +1,38 @@
 package subscription
 
 import (
+	"fmt"
 	"github.com/300brand/logger"
 	"net/http"
 	"net/url"
 )
 
 type CookieJar struct {
-	cookies []http.Cookie
+	cookies map[string]http.Cookie
 }
 
 var _ http.CookieJar = new(CookieJar)
 
 func NewCookieJar() (jar *CookieJar) {
 	return &CookieJar{
-		cookies: make([]http.Cookie, 0, 4096),
+		cookies: make(map[string]http.Cookie),
 	}
 }
 
 func (jar *CookieJar) SetCookies(u *url.URL, cookies []*http.Cookie) {
 	logger.Debug.Printf("CookieJar.SetCookie: %s (%d)", u, len(cookies))
-	for _, cookie := range cookies {
-		jar.cookies = append(jar.cookies, *cookie)
+	for _, c := range cookies {
+		key := fmt.Sprintf("%s|%s|%s", c.Domain, c.Path, c.Name)
+		jar.cookies[key] = *c
 	}
 }
 
 func (jar *CookieJar) Cookies(u *url.URL) (cookies []*http.Cookie) {
 	logger.Debug.Printf("CookieJar.Cookies: %s", u)
+	cookies = make([]*http.Cookie, 0, len(jar.cookies))
 	for i, c := range jar.cookies {
-		logger.Debug.Printf("[%03d] %s=%s; Expires=%s; Path=%s", i, c.Name, c.Value, c.Expires, c.Path)
+		logger.Debug.Printf("[%s] %s=%s; Expires=%s; Path=%s", i, c.Name, c.Value, c.RawExpires, c.Path)
+		cookies = append(cookies, &c)
 	}
-	return []*http.Cookie{}
+	return
 }
